4da9d45955314d1b507e21e47a4149236439ebf0
HyunJun Baek <hjbaek5796@gmail.com>
2024-08-20 17:10:36 +0900

    Implement math operator functions

---
+++ script.js
@@ -0,0 +1,16 @@
+// math operator functions
+function calcAdd(a, b) {
+  return a + b;
+}
+
+function calcSub(a, b) {
+  return a - b;
+}
+
+function calcMult(a, b) {
+  return a * b;
+}
+
+function calcMult(a, b) {
+  return a / b;
+}
